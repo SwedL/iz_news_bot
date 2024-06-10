eacf4c9d82f9fba753849ab45458e3a808c12e05
Alexey Osminin <supersega@mail.ru>
2024-06-10 22:06:14 +0500

    create keyboard

--- core/keyboards/reply.py
+++ core/keyboards/reply.py
@@ -1,25 +1,64 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+# reply_keyboard = ReplyKeyboardMarkup(keyboard=[
+#     [
+#         KeyboardButton(text='Очистить рубрики'),
+#     ],
+#     [
+#         KeyboardButton(text='Мир'),
+#         KeyboardButton(text='Общество'),
+#         KeyboardButton(text='ЧП'),
+#         KeyboardButton(text='Здоровье'),
+#         KeyboardButton(text='Армия'),
+#
+#     ],
+#     [
+#         KeyboardButton(text='Экономика'),
+#         KeyboardButton(text='Политика'),
+#         KeyboardButton(text='Недвижимость'),
+#         KeyboardButton(text='Авто'),
+#         KeyboardButton(text='Культура'),
+#
+#     ],
+# [
+#         KeyboardButton(text='Пресс-Релизы'),
+#         KeyboardButton(text='Спорт'),
+#         KeyboardButton(text='Наука и техника'),
+#         KeyboardButton(text='Туризм'),
+#     ],
+# ], resize_keyboard=True)
+
 reply_keyboard = ReplyKeyboardMarkup(keyboard=[
     [
         KeyboardButton(text='Очистить рубрики'),
     ],
     [
-        KeyboardButton(text='Мир'),
-        KeyboardButton(text='Общество'),
-        KeyboardButton(text='Происшествия'),
+        KeyboardButton(text='🌍'),
+        KeyboardButton(text='👨‍👩‍👧‍👦'),
+        KeyboardButton(text='🚔'),
         KeyboardButton(text='💊'),
-        KeyboardButton(text='Армия'),
+        KeyboardButton(text='🪖'),
 
     ],
     [
-        KeyboardButton(text='Экономика'),
-        KeyboardButton(text='Политика'),
-        KeyboardButton(text='Недвижимость'),
-        KeyboardButton(text='Авто'),
-        KeyboardButton(text='Культура'),
+        KeyboardButton(text='💵'),
+        KeyboardButton(text='💼'),
+        KeyboardButton(text='🏙'),
+        KeyboardButton(text='🚗'),
+        KeyboardButton(text='🎼'),
 
+    ],
+[
+        KeyboardButton(text='📰'),
+        KeyboardButton(text='⚽️'),
+        KeyboardButton(text='🔭'),
+        KeyboardButton(text='⛱'),
     ],
 ], resize_keyboard=True)
 
+m = {
+    'Мир': '🌍', 'Общество': '👨‍👩‍👧‍👦', 'ЧП': '🚔', 'Здоровье': '💊', 'Армия': '🪖',
+    'Экономика': '💵', 'Политика': '💼', 'Недвижимость': '🏙', 'Авто': '🚗', 'Культура': '🎼',
+    'Пресс-Релизы': '📰', 'Спорт': '⚽️', 'Наука и техника': '🔭', 'Туризм': '⛱',
+}
 
