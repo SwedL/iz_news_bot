f2bee744ef63d72bfc4c0429fac3843901bf4e17
Alexey Osminin <supersega@mail.ru>
2024-06-12 11:10:44 +0500

    create main class IzNews

--- main.py
+++ main.py
@@ -9,54 +9,62 @@ import logging
 from dotenv import load_dotenv
 from aiogram.filters import Command
 from aiogram import F
+from news_sources.iz_news_source import IZNewsSource
 
 load_dotenv()
 
-
-async def start_bot(bot: Bot):
-    await bot.send_message(settings.bots.admin_id, text='Бот запущен!')
-
-
-async def stop_bot(bot: Bot):
-    await bot.send_message(settings.bots.admin_id, text='Бот остановлен!')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - [%(levelname)s] - %(name)s - "
+           "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+)
 
 
-async def start():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - [%(levelname)s] - %(name)s - "
-               "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
-    )
-    bot = Bot(token=settings.bots.bot_token)
+class IzNews:
+    def __init__(self):
+        self.bot = Bot(token=settings.bots.bot_token)
+        self.source = IZNewsSource()
 
-    dp = Dispatcher()
-    dp.startup.register(start_bot)
-    dp.shutdown.register(stop_bot)
-    dp.message.register(get_photo, F.photo)
-    dp.message.register(get_start, Command(commands=['start', 'run']))
+    async def start_bot(self):
+        await self.bot.send_message(settings.bots.admin_id, text='Бот запущен!')
 
-    try:
-        await dp.start_polling(bot)
-    finally:
-        await bot.session.close()
+    async def stop_bot(self):
+        await self.bot.send_message(settings.bots.admin_id, text='Бот остановлен!')
 
-    chat_id = os.getenv('CHANNEL_ID')
-    # source = IZNewsSource()
-    # source.get_news()
-    # news_to_post = source.list_processed_news
+    async def start(self):
+        dp = Dispatcher()
+        dp.startup.register(self.start_bot)
+        dp.shutdown.register(self.stop_bot)
+        dp.message.register(get_photo, F.photo)
+        dp.message.register(get_start, Command(commands=['start', 'run']))
 
-    # for news in news_to_post:
-    #     photo = requests.get(news.get('image_url', source.DEFAULT_IMAGE_URL)).content
-    #     caption = source.caption_message(news)
-    #
-    #     bot.send_photo(
-    #         chat_id=chat_id,
-    #         photo=photo,
-    #         caption=caption,
-    #     )
-    #     print(f'Новость {news["summary"]} была отправлена')
-    #     time.sleep(5)
+        try:
+            await dp.start_polling(self.bot)
+        finally:
+            await self.bot.session.close()
+
+        chat_id = os.getenv('CHANNEL_ID')
+        # source = IZNewsSource()
+        # source.get_news()
+        # news_to_post = source.list_processed_news
+
+        # for news in news_to_post:
+        #     photo = requests.get(news.get('image_url', source.DEFAULT_IMAGE_URL)).content
+        #     caption = source.caption_message(news)
+        #
+        #     bot.send_photo(
+        #         chat_id=chat_id,
+        #         photo=photo,
+        #         caption=caption,
+        #     )
+        #     print(f'Новость {news["summary"]} была отправлена')
+        #     time.sleep(5)
 
 
 if __name__ == "__main__":
-    asyncio.run(start())
+    iz = IzNews()
+    try:
+        asyncio.run(iz.start())
+    except:
+        pass
+
